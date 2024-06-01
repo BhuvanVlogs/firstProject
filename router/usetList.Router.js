c40aa3098868688e05f154839bfd47b39c66b351
Bhuvaneswaran Selvaraj <bhuvaneswaranppm@gmail.com>
2024-06-01 22:34:13 +0530

    create Sample API's

---
+++ router/usetList.Router.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const { addUser } = require("../controller/usetList.controller")
+
+router.get('/', addUser)
+
+module.exports = router;
